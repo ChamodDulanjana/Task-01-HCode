d644242f71fb55514a1ed8ea2c330a20dd350139
ChamodDulanjana <chamodperera128@gmail.com>
2024-09-04 13:46:46 +0530

    Create post request

--- Front End/src/App.tsx
+++ Front End/src/App.tsx
@@ -1,23 +1,38 @@
 import { useState } from 'react'
+import axios from "axios";
 
 function App() {
     const [selectedFile, setSelectedFile] = useState(null);
 
-    const handleClick = () => {
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+
+    const handleClick = async () => {
         if (!selectedFile) {
             alert("Please select a file");
-            return;
         }
+
+        try {
+            const response = await axios.post(
+                "http://localhost:8080/api/v1/files",
+                JSON.stringify(formData),
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    }
+                }
+            );
+            console.log(response.data);
+        } catch (error) {
+            console.log("There was an error", error);
+        }
+
     }
 
     const handleFileChange = (e) => {
         setSelectedFile(e.target.files[0]);
     }
 
-    const formData = new FormData();
-    formData.append("file", selectedFile);
-    console.log(formData);
-    console.log(selectedFile);
 
   return (
     <div className="flex flex-col items-center h-screen bg-[#f1f2f6] gap-20">
